9f536443039424dcd372ce8314144e6f81445f59
Brennan Ashton <bashton@brennanashton.com>
2013-07-12 09:39:19 -0700

    Update comments and add additional file to read

--- src/indexer.c
+++ src/indexer.c
@@ -1,7 +1,5 @@
 /*
  * indexer.c
- *
- *  Created on: Jul 12, 2013
  *      Author: Brennan
  */
 
@@ -10,7 +8,7 @@
  ============================================================================
  Name        : indexer.c
  Author      : Brennan Ashton
- Version     : 0.1
+ Version     : 0.2
  Description : Code Sample. Print an index of words in files provided
  ============================================================================
  */
@@ -19,22 +17,28 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define BUFLEN 100  //This is something to start with, it will allocate more if needed
-#define WORDDEL " "  //Delimiter
+#define BUFLEN 100  //This is something to start with, it will allocate more
+					//if needed
 
-//We use linked lists to sort and store data
+#define WORDDEL " ~!@#$%^&*()_+`-={}|[]\\:\";'<>?,./" //Delimiters
+
+//This is the base struct for linked lists
 typedef struct LinkedList {
 	void *data;
 	struct LinkedList *nextelm;
 } linkedlist;
 
-//Data in a linkedlist for a word
+//It is a pain to refer to the elements always as data and nextelm
+//It kills the readability of the code, however we can get away with
+//creating additional types in the same format, much like a subclass
+
+//A linked list type for holding words and the associated files
 typedef struct WordStr {
 	char *word;
 	struct LinkedList *files;
 } word_str;
 
-//Data in a linkedlist for a file
+//A linked list type for holding files and the associated lines
 typedef struct FilesStr {
 	char *filename;
 	struct LinkedList *lines;
@@ -43,16 +47,23 @@ typedef struct FilesStr {
 //Prototypes
 linkedlist* list_start(void *data);
 linkedlist * list_add(linkedlist *oldnode, void *data);
-linkedlist* merge(linkedlist *head_one, linkedlist *head_two, int (*comp)(linkedlist*,linkedlist*));
+linkedlist* merge(linkedlist *head_one, linkedlist *head_two,
+		int (*comp)(linkedlist*,linkedlist*));
 int linecmp(linkedlist *list1, linkedlist *list2);
 int filenmcmp(linkedlist *list1, linkedlist *list2);
 int wordcmp(linkedlist *list1, linkedlist *list2);
 linkedlist* mergesort(linkedlist *head,int (*comp)(linkedlist*,linkedlist*));
 void filemergepost(linkedlist *currlist);
 void wordmergepost(linkedlist *currlist);
-linkedlist* mergelist(linkedlist *wlist,int (*comp)(linkedlist*,linkedlist*), void (*posttask)(linkedlist*));
+linkedlist* mergelist(linkedlist *wlist,int (*comp)(linkedlist*,linkedlist*),
+		void (*posttask)(linkedlist*));
 
-//Used to create a generic linkedlist
+/**
+ * linkedlist* list_start(void *data) - Used to generate a linked list
+ * @data:	Pointer to data payload for first element.
+ *
+ * @return: Returns the created linked list element
+ */
 linkedlist* list_start(void *data)
 {
 	linkedlist *node;
@@ -66,32 +77,60 @@ linkedlist* list_start(void *data)
 	node->nextelm = NULL;
 	return node;
 }
-//Used to add items to a generic linkedlist
+
+/**
+ * linkedlist * list_add(linkedlist *oldnode, void *data)
+ * 			Used to add a node to an existing linked list
+ * 			New node is placed at top of the list
+ *
+ * @oldnode:	Pointer to current top node of linked list.
+ * @data:		Pointer to payload data of new node.
+ *
+ * @return: Returns the new top node
+ */
 linkedlist * list_add(linkedlist *oldnode, void *data)
 {
 	linkedlist *node;
 	node = (linkedlist *)malloc(sizeof(linkedlist));
 	if(node == NULL)
 	{
-		printf("Could not allocate mem!");
+		printf("Could not allocate mem for new linked list node!\n");
 		exit(-1);
 	}
 	node->data=(void*)data;
 	node->nextelm = oldnode;
 	return node;
 }
-//Perform the merge operation for merge sort, takes in the two linked list as well as a comparison function
-linkedlist* merge(linkedlist *head_one, linkedlist *head_two, int (*comp)(linkedlist*,linkedlist*))
+
+/**
+ * linkedlist* merge(linkedlist *head_one, linkedlist *head_two,
+ * 				     int (*comp)(linkedlist*,linkedlist*))
+ *
+ * 			Used to merge two linked lists
+ *
+ * @headone:	Pointer to first linked list head.
+ * @headtwo:	Pointer to second linked list head.
+ * @comp:		This function allows for custom comparison
+ * 				functions to be used that take in linked lists
+ * 				and return <1 if headone is less than head two
+ * 				and return >1 if headone is greater than head two
+ *
+ * @return: Returns the new merged top node
+ */
+linkedlist* merge(linkedlist *head_one, linkedlist *head_two,
+				  int (*comp)(linkedlist*,linkedlist*))
 {
 	linkedlist *head_three;
 
+	//if any one of the linked lists are empty just return the other
 	if(head_one == NULL)
 		return head_two;
 
 	if(head_two == NULL)
 		return head_one;
 
-//if(strcmp(head_one->word, head_two->word)<0)
+	//Example for a string comparison
+	//if(strcmp(head_one->word, head_two->word)<0)
 	if(((*comp)(head_one, head_two))<0)
 	{
 		head_three = head_one;
@@ -102,22 +141,77 @@ linkedlist* merge(linkedlist *head_one, linkedlist *head_two, int (*comp)(linked
 	}
 	return head_three;
 }
-//comparison function used to compare line numbers can be passed to merge functions
+
+/**
+ * int linecmp(linkedlist *list1, linkedlist *list2)
+ *
+ * 			Used as a comparison function for line numbers
+ * 			for a linked list merge operation.
+ *
+ * @list1:	Pointer to first linked list head.
+ * @list2:	Pointer to second linked list head.
+ * @return:	Return <1 if head one is less than head two
+ * 			Return >1 if head one is greater than head two
+ *
+ */
 int linecmp(linkedlist *list1, linkedlist *list2)
 {
 	return(*((int*)list1->data)-*((int*)list2->data));
 }
-//comparison function used to compare filenames can be passed to merge functions
+
+/**
+ * int filenmcmp(linkedlist *list1, linkedlist *list2)
+ *
+ * 			Used as a comparison function for file names
+ * 			for a linked list merge operation.
+ *
+ * @list1:	Pointer to first linked list head.
+ * @list2:	Pointer to second linked list head.
+ * @return:	Return <1 if head one is less than head two
+ * 			Return >1 if head one is greater than head two
+ *			Comparison is executed based on strcmp
+ *
+ */
 int filenmcmp(linkedlist *list1, linkedlist *list2)
 {
-	return(strcmp(((file_str *)list1->data)->filename,((file_str *)list2->data)->filename));
+	return(strcmp(((file_str *)list1->data)->filename,
+			((file_str *)list2->data)->filename));
 }
-//comparison function used to compare words can be passed to merge functions
+
+/**
+ * int wordcmp(linkedlist *list1, linkedlist *list2)
+ *
+ * 			Used as a comparison function for words
+ * 			for a linked list merge operation.
+ *
+ * @list1:	Pointer to first linked list head.
+ * @list2:	Pointer to second linked list head.
+ * @return:	Return <1 if head one is less than head two
+ * 			Return >1 if head one is greater than head two
+ *			Comparison is executed based on strcmp
+ *
+ * @return: Returns the new top node
+ */
 int wordcmp(linkedlist *list1, linkedlist *list2)
 {
-	return(strcmp(((word_str *)list1->data)->word,((word_str *)list2->data)->word));
+	return(strcmp(((word_str *)list1->data)->word,
+			((word_str *)list2->data)->word));
 }
-//perform a mergesort on a linked list, pass the comparison function to be used
+
+/**
+ * linkedlist* mergesort(linkedlist *head,int (*comp)(linkedlist*,linkedlist*))
+ *
+ * 			Merge sort function for linked lists
+ *
+ * @list1:	Pointer to linked list head.
+ *
+ * @comp:		This function allows for custom comparison
+ * 				functions to be used that take in linked lists
+ * 				and return <1 if node 1 is less than head two
+ * 				and return >1 if node 2 is greater than head two
+ *
+ * @return: Returns the new top node of sorted linked list
+ */
 linkedlist* mergesort(linkedlist *head,int (*comp)(linkedlist*,linkedlist*))
 {
 	linkedlist *head_one;
@@ -137,22 +231,63 @@ linkedlist* mergesort(linkedlist *head,int (*comp)(linkedlist*,linkedlist*))
 
 	return merge(mergesort(head_one,(comp)), mergesort(head_two,(comp)),(comp));
 }
-//When merging file listings we needs to perform some clean up on the line lists
+
+/**
+ * filemergepost(linkedlist *currlist)
+ *
+ * 			Execute post tasks for sort on file names
+ * @list1:	Pointer to linked list head.
+ *
+ *		When merging file listings we needs to perform
+ *		some clean up on the line lists
+ */
 void filemergepost(linkedlist *currlist)
 {
 	//sort the line numbers
-	((file_str*)currlist->data)->lines = mergesort(((file_str*)currlist->data)->lines,linecmp);
+	((file_str*)currlist->data)->lines =
+			mergesort(((file_str*)currlist->data)->lines,linecmp);
 }
-//when merging word listings we need to perform some clean up on the file lists
+
+/**
+ * wordmergepost(linkedlist *currlist)
+ *
+ * 			Execute post tasks for sort on file names
+ * @list1:	Pointer to linked list head.
+ *
+ *		When merging word listings we needs to perform
+ *		some clean up on the file lists
+ */
 void wordmergepost(linkedlist *currlist)
 {
 	//sort the files after a merge
-	((word_str*)currlist->data)->files = mergesort(((word_str*)currlist->data)->files,filenmcmp);
+	((word_str*)currlist->data)->files =
+			mergesort(((word_str*)currlist->data)->files,filenmcmp);
 	//merge any files that are the same
-	((word_str*)currlist->data)->files = mergelist(((word_str*)currlist->data)->files,filenmcmp,filemergepost);
+	((word_str*)currlist->data)->files =
+			mergelist(((word_str*)currlist->data)->files,filenmcmp,filemergepost);
 }
-//Generic function used to merge dup listings in a sorted linked list,  pass the function for finding dups
-linkedlist* mergelist(linkedlist *wlist,int (*comp)(linkedlist*,linkedlist*), void (*posttask)(linkedlist*))
+
+/**
+ * linkedlist* mergelist(linkedlist *wlist,int (*comp)(linkedlist*,linkedlist*),
+ * 						 void (*posttask)(linkedlist*))
+ *
+ * 			Merge duplicate listings when a comparison function says
+ * 			the two elements are the same.	Allow for a post function
+ * 			to be run after each combine.
+ *
+ * @wlist:	Pointer to linked list head.
+ *
+ * @comp:		This function allows for custom comparison
+ * 				functions to determine if the elements are
+ * 				the same. A return of 0 indicates they are
+ * 				the same.
+ *
+ * @posttask:   A pointer to a function that should be run once
+ * 				a combine action has been run. The node of the
+ * 				last combined list is provided
+ */
+linkedlist* mergelist(linkedlist *wlist,int (*comp)(linkedlist*,linkedlist*),
+		void (*posttask)(linkedlist*))
 {
 	linkedlist *currlist = wlist;
 	while(currlist !=NULL){
@@ -176,6 +311,7 @@ linkedlist* mergelist(linkedlist *wlist,int (*comp)(linkedlist*,linkedlist*), vo
 	}
 	return wlist;
 }
+
 //Format the final index nicely
 void display(linkedlist *wlist)
 {
@@ -185,14 +321,20 @@ void display(linkedlist *wlist)
 		printf("%s\n",((word_str*)wlist->data)->word);
 		while(((word_str*)wlist->data)->files)
 		{
-			printf("\t%s",((file_str*)((word_str*)wlist->data)->files->data)->filename);
+			printf("\t%s",
+					((file_str*)((word_str*)wlist->data)->files->data)->filename);
 			while(((file_str*)((word_str*)wlist->data)->files->data)->lines)
 			{
-				printf(" %d",*((int*)((file_str*)((word_str*)wlist->data)->files->data)->lines->data));
-				((file_str*)((word_str*)wlist->data)->files->data)->lines = ((file_str*)((word_str*)wlist->data)->files->data)->lines->nextelm;
+				printf(" %d",
+						*((int*)((file_str*)(
+								(word_str*)wlist->data)->files->data)->lines->data));
+				((file_str*)((word_str*)wlist->data)->files->data)->lines =
+						((file_str*)((word_str*)wlist->data)->files->data)->
+						lines->nextelm;
 			}
 			printf("\n");
-			((word_str*)wlist->data)->files = ((word_str*)wlist->data)->files->nextelm;
+			((word_str*)wlist->data)->files =
+					((word_str*)wlist->data)->files->nextelm;
 		}
 		//move to next word
 		wlist = wlist->nextelm;
@@ -234,11 +376,13 @@ int main ( int argc, char *argv[] ) {
 			linebuf = (char*)malloc(sizeof(char)*buflen);
 			if(linebuf == NULL)
 			{
-				printf("Could not allocate mem!");
+				printf("Could not allocate mem to read line from file!\n");
 				return(-1);
 			}
 			fgets(linebuf,sizeof(char)*BUFLEN,fp);
-			//was if there was not enough buffer length tack on some more and read more of the file
+			//if there was not enough buffer length tack
+			//on some more and read more of the file
+
 			while(1)
 			{
 				if(!(feof(fp)||(linebuf[strlen(linebuf)-1]=='\n')))
@@ -247,7 +391,11 @@ int main ( int argc, char *argv[] ) {
 					linebuf = (char*)realloc(linebuf,sizeof(char)*buflen);
 					if(linebuf == NULL)
 					{
-						printf("Could not allocate mem!");
+						//The line was too long to read
+						//With some refactoring this operation could be split
+						//into multiple parsing.  Issue is insuring a word is not
+						//cut off
+						printf("Could not allocate mem to read line from file!\n");
 						return(-1);
 					}
 				}else{
@@ -255,11 +403,12 @@ int main ( int argc, char *argv[] ) {
 				}
 				if(fgets(&linebuf[buflen-BUFLEN],sizeof(char)*BUFLEN,fp)==NULL)
 				{
-					//we reached eof and no data was read make sure that word is cleared nice
+					//we reached eof and no data was read make sure that word
+					//is cleared nice
 					word[0]='\n';
 				}
 			}
-			//break up the line into words
+			//break up the line into words based on delimeter string
 			word = strtok(linebuf, WORDDEL);
 			while(word != NULL &&!feof(fp))
 			{
@@ -276,13 +425,13 @@ int main ( int argc, char *argv[] ) {
 				filestr = (file_str*)malloc(sizeof(file_str));
 				if(filestr == NULL)
 				{
-					printf("Could not allocate mem!");
+					printf("Could not allocate mem to hold list for file name!\n");
 					return(-1);
 				}
 				filestr->filename=(char*)malloc(sizeof(char)*strlen(argv[i]));
 				if(filestr->filename == NULL)
 				{
-					printf("Could not allocate mem!");
+					printf("Could not allocate mem to store file name!\n");
 					return(-1);
 				}
 				strcpy(filestr->filename,argv[i]);
@@ -295,13 +444,13 @@ int main ( int argc, char *argv[] ) {
 				wordstr = (word_str*)malloc(sizeof(word_str));
 				if(wordstr == NULL)
 				{
-					printf("Could not allocate mem!");
+					printf("Could not allocate mem for word list storage!\n");
 					return(-1);
 				}
 				wordstr->word = (char*)malloc(sizeof(char)*strlen(word));
 				if(wordstr->word == NULL)
 				{
-					printf("Could not allocate mem!");
+					printf("Could not allocate mem to store word!\n");
 					return(-1);
 				}
 				strcpy(wordstr->word,word);
